0f28da6aedd979b670af2bc541b78139a38367c7
Joly Axel <joly.axel@outlook.fr>
2016-12-30 21:48:19 +0100

    ajout du trajet fonctionnel, sauf pour start/end

--- src/AppBundle/Controller/AddTravelController.php
+++ src/AppBundle/Controller/AddTravelController.php
@@ -49,7 +49,10 @@ class AddTravelController extends Controller
 
         $travel = new Travel();
 
-        $form = $this->createFormBuilder($travel)->add('date',DateTimeType::class, array(
+        $form = $this->createFormBuilder($travel)
+
+
+            ->add('date',DateTimeType::class, array(
             'date_widget' => "single_text",
             'time_widget' => "single_text",
             'label' => 'Date',
@@ -70,32 +73,30 @@ class AddTravelController extends Controller
                 'choice_label' => function ($value) {return strtoupper($value);},
                 'attr' => ['class' => 'browser-default']
             ))
-            ->add('start',ChoiceType::class, array(
-                    'choices' => $tests, 'attr' => ['class' => 'browser-default']
-            ))
 
-            ->add('end',ChoiceType::class)
             ->add('description',TextType::class)
             ->add('valider', SubmitType::class, array('label' => 'Proposer le trajet', 'attr' => ['class' => 'btn waves-effect waves-light']))->getForm();
-
-
-
 
 
         $form->handleRequest($request);
 
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $travel->setDriver($user);
+            $start = $request->get('start');
+            $end = $request->get('end');
 
+            $travel->setStart($this->getDoctrine()->getRepository('AppBundle:Travel')->find($start));
+            $travel->setEnd($this->getDoctrine()->getRepository('AppBundle:Travel')->find($end));
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($travel);
             $em->flush();
-
-            return $this->redirectToRoute('home', array('user' => $user, 'city' => $city));
+            $travel = $this->getDoctrine()->getRepository('AppBundle:Travel')->findAll();
+            return $this->redirectToRoute('home', array('user' => $user, 'travel' => $travel));
         }
 
-        return $this->render('AppBundle:Travel:travelform.html.twig', array('user' => $user, 'form' => $form->createView()));
+        return $this->render('AppBundle:Travel:travelform.html.twig', array('city' => $city, 'user' => $user, 'form' => $form->createView()));
     }
 
 
