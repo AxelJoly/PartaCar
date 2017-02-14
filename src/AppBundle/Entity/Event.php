50157abd787396edb3c77ef14adf1a22499cd9ae
Benjamin <benjamin.revertegat@isen.yncrea.fr>
2017-02-14 10:23:03 +0100

    Save début changement majeur

---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Event
+ *
+ * @ORM\Table(name="event")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventRepository")
+ */
+class Event
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lieu", type="string", length=500)
+     */
+    private $lieu;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=1000, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="responsableEvent_mail", referencedColumnName="mail")
+     */
+    private $responsableEvent;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Travel", mappedBy="event")
+     */
+    private $travels;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Event
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set lieu
+     *
+     * @param string $lieu
+     *
+     * @return Event
+     */
+    public function setLieu($lieu)
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    /**
+     * Get lieu
+     *
+     * @return string
+     */
+    public function getLieu()
+    {
+        return $this->lieu;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Event
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Event
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->travels = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Set responsableEvent
+     *
+     * @param \AppBundle\Entity\User $responsableEvent
+     *
+     * @return Event
+     */
+    public function setResponsableEvent(\AppBundle\Entity\User $responsableEvent = null)
+    {
+        $this->responsableEvent = $responsableEvent;
+
+        return $this;
+    }
+
+    /**
+     * Get responsableEvent
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getResponsableEvent()
+    {
+        return $this->responsableEvent;
+    }
+
+    /**
+     * Add travel
+     *
+     * @param \AppBundle\Entity\Travel $travel
+     *
+     * @return Event
+     */
+    public function addTravel(\AppBundle\Entity\Travel $travel)
+    {
+        $this->travels[] = $travel;
+
+        return $this;
+    }
+
+    /**
+     * Remove travel
+     *
+     * @param \AppBundle\Entity\Travel $travel
+     */
+    public function removeTravel(\AppBundle\Entity\Travel $travel)
+    {
+        $this->travels->removeElement($travel);
+    }
+
+    /**
+     * Get travels
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTravels()
+    {
+        return $this->travels;
+    }
+}
